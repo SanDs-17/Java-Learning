e89de3aec9985fa40b10d00d414ee1e72eb991ab
SanDs-17 <srisanthosh057@gmail.com>
2025-03-28 15:42:19 +0530

    Updated Java files and removed VS Code settings

---
+++ ArrayExample.java
@@ -0,0 +1,10 @@
+class ArrayExample{
+    public static void main(String args[]){
+        int[] Numbers={10,20,30,40,50,60,70,80,90,100};
+
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(Numbers[i]);
+        }
+    }
+}
